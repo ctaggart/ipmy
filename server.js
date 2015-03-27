5d6f2256535ff8e54538178032122f9196bda638
Cameron Taggart <cameron@tachyus.com>
2015-03-27 15:04:35 -0700

    return the remote IP address

--- server.js
+++ server.js
@@ -2,7 +2,10 @@
 var http = require('http');
 http.createServer(function (req, res) {
     res.writeHead(200, {
-        'Content-Type': 'text/html'
+        'Content-Type': 'application/json'
     });
-    res.end('Hello from Azure running node version: ' + process.version + '</br>');
+    var ip = {
+        ip: req.connection.remoteAddress
+    };
+    res.end(JSON.stringify(ip));
 }).listen(process.env.PORT || 3000);
--- server.ts
+++ server.ts
@@ -1,7 +1,12 @@
 /// <reference path="typings/node/node.d.ts"/>
 
+type IpRsp = {
+  ip: string;
+}
+
 import http = require('http');
-http.createServer((req,res) => {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello from Azure running node ersion: ' + process.version + '</br>');
+http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'application/json'});
+  let ip: IpRsp = {ip: req.connection.remoteAddress}
+  res.end(JSON.stringify(ip));
 }).listen(process.env.PORT || 3000);
